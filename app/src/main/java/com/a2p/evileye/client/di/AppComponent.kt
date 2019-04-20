9dcf62c0f7bf4a631e697bbf419baab3fee5e255
yt8492 <tmymyut2000@gmail.com>
2019-04-21 00:27:29 +0900

    add dagger application

---
+++ app/src/main/java/com/a2p/evileye/client/di/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.a2p.evileye.client.di
+
+import com.a2p.evileye.client.App
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<App> {
+    override fun inject(application: App)
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: App): Builder
+
+        fun build(): AppComponent
+    }
+}
