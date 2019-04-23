1ba76a3422c88ea8e455d3d2a87b948860c94651
yt8492 <tmymyut2000@gmail.com>
2019-04-23 22:53:59 +0900

    implement login

--- app/src/main/java/com/a2p/evileye/client/data/user/UserRepository.kt
+++ app/src/main/java/com/a2p/evileye/client/data/user/UserRepository.kt
@@ -1,23 +1,40 @@
 package com.a2p.evileye.client.data.user
 
 import android.content.Context
+import android.util.Log
 import com.yt8492.evileye.protobuf.Empty
+import com.yt8492.evileye.protobuf.LoginRequest
 import com.yt8492.evileye.protobuf.PublicGrpc
 import io.grpc.ManagedChannel
-import java.lang.Exception
 
 class UserRepository(private val context: Context,
-                     private val channel: ManagedChannel) : UserDataSource {
+                     channel: ManagedChannel) : UserDataSource {
     private val sharedPreferences by lazy {
         context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
     }
 
+    private val stub = PublicGrpc.newBlockingStub(channel)
+
     override fun checkConnection(result: (Boolean) -> Unit) {
-        val stub = PublicGrpc.newBlockingStub(channel)
         val req = Empty.newBuilder().build()
         try {
             val res = stub.healthCheck(req)
             result(res.commitHash.isNotBlank())
+        } catch (e: Exception) {
+            e.printStackTrace()
+            result(false)
+        }
+    }
+
+    override fun login(userName: String, password: String, result: (Boolean) -> Unit) {
+        val req = LoginRequest.newBuilder()
+            .setScreenName(userName)
+            .setPassword(password)
+            .build()
+        try {
+            val res = stub.login(req)
+            Log.d(TAG, "token: ${res.token}")
+            result(true)
         } catch (e: Exception) {
             e.printStackTrace()
             result(false)
@@ -31,5 +48,6 @@ class UserRepository(private val context: Context,
     companion object {
         private const val PREF_NAME = "EVIL_EYE"
         private const val KEY_TOKEN = "KEY_TOKEN"
+        private const val TAG = "UserRepository"
     }
 }
