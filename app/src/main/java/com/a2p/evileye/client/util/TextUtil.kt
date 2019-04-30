464487902cd7388f374d02de3318b3bfcdd9fd7b
yt8492 <tmymyut2000@gmail.com>
2019-04-30 13:38:52 +0900

    fix url bug

--- app/src/main/java/com/a2p/evileye/client/util/TextUtil.kt
+++ app/src/main/java/com/a2p/evileye/client/util/TextUtil.kt
@@ -1,7 +1,12 @@
 package com.a2p.evileye.client.util
 
 fun String.hideWayback(): String {
-    val regex = """https?://""".toRegex()
-    val prefix = regex.findAll(this).map(MatchResult::value).toList()[1]
-    return prefix + this.split(regex).drop(2).joinToString("")
+    return try {
+        val regex = """https?://""".toRegex()
+        val prefix = regex.findAll(this).map(MatchResult::value).toList()[1]
+        prefix + this.split(regex).drop(2).joinToString("")
+    } catch (e: Exception) {
+        e.printStackTrace()
+        this
+    }
 }
